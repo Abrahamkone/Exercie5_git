df431866f2b4e1e609612a35434194890c75b1e7
CLIM\2335932 <2335932@cegeplimoilou.ca>
2025-03-07 15:52:58 -0500

    COMMIT comflit resolu dd

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,7 @@ public class Main {
         }
         System.out.println(" ");
         System.out.println("Conflit avec Leslie");
+        System.out.println("Conflit avec Leslie resolu");
+    }
     }
 }
